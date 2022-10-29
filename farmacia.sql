cdac1c5160a800baf8e8ef47337ce5d7b35ff6cd
jesuchy <jcharrymeza1@gmail.com>
2022-10-29 12:26:03 -0500

    subiendo base de datos

--- farmacia.sql
+++ farmacia.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-07-2021 a las 21:14:19
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.11
+-- Tiempo de generación: 29-10-2022 a las 19:24:52
+-- Versión del servidor: 10.1.29-MariaDB
+-- Versión de PHP: 7.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `blog1`
+-- Base de datos: `farmacia`
 --
 
 -- --------------------------------------------------------
