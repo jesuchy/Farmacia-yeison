ec6a6554cdaa0876c46cdca1d6a3d0438aa69efe
jesuchy <jcharrymeza1@gmail.com>
2022-11-09 00:45:23 -0500

    Subiendo proyecto final

--- public/SQL/compra.sql
+++ public/SQL/compra.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-11-2022 a las 02:34:56
+-- Tiempo de generación: 09-11-2022 a las 06:44:40
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `tbl_personal` (
   `tbl_personal_ID` int(11) NOT NULL,
   `tbl_personal_NOMBRE` varchar(30) NOT NULL,
-  `tbl_personal_APELLIDO` varchar(30) NOT NULL,
+  `tbl_personal_APELLIDO` varchar(20) NOT NULL,
   `tbl_personal_TIPO_DOCUMENTO` varchar(10) NOT NULL,
   `tbl_personal_DOCUMENTO` varchar(20) NOT NULL,
   `tbl_personal_EMPRESA` varchar(30) NOT NULL,
@@ -45,7 +45,10 @@ CREATE TABLE `tbl_personal` (
 --
 
 INSERT INTO `tbl_personal` (`tbl_personal_ID`, `tbl_personal_NOMBRE`, `tbl_personal_APELLIDO`, `tbl_personal_TIPO_DOCUMENTO`, `tbl_personal_DOCUMENTO`, `tbl_personal_EMPRESA`, `tbl_personal_DIRECCION`, `tbl_personal_CELULAR`, `tbl_personal_CORREO`, `tbl_personal_PASSWORD`) VALUES
-(2, 'jeus', 'Chaeey', 'xx', 'Documento', 'Empresa', 'Direccion', 'Celular', 'CorreoElectronico', 'Contra');
+(2, 'jeus', '', 'xx', 'Documento', 'Empresa', 'Direccion', 'Celular', 'CorreoElectronico', 'Contra'),
+(3, 'Jesus', 'charry', 'CC', '1005681522', 'red', 'hjdhdbf', '3018765432', 'usuario', 'charry'),
+(4, 'Jesus', 'Charry', 'CC', '1005681522', 'red', 'dfdfsdf', '3018765432', 'j@gail.com', 'chatt'),
+(5, 'jesus ', 'charry', 'CE', '1005681522', 'red', 'dfdfsdf', '3018765432', 'j@gail.com', 'cjdnndjkndj');
 
 --
 -- Índices para tablas volcadas
@@ -65,7 +68,7 @@ ALTER TABLE `tbl_personal`
 -- AUTO_INCREMENT de la tabla `tbl_personal`
 --
 ALTER TABLE `tbl_personal`
-  MODIFY `tbl_personal_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `tbl_personal_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
